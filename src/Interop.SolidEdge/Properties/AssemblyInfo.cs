a73cec433c08b6ce89be93939d4c62eb8f5d03e9
Jason Newell <jason.newell@outlook.com>
2016-01-04 21:37:05 -0600

    Solid Edge ST8 MP3 release.

--- src/Interop.SolidEdge/Properties/AssemblyInfo.cs
+++ src/Interop.SolidEdge/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("108.0.0.0")]
 
 // This version should match the NuGet version.
-[assembly: AssemblyFileVersion("108.2.0.0")]
+[assembly: AssemblyFileVersion("108.3.0.0")]
