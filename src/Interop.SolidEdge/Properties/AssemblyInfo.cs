866cfc0b6a311345e4c02a5b6a90de97feefd810
Jason Newell <jlnewell@users.noreply.github.com>
2017-10-25 23:07:38 -0500

    Solid Edge ST9 MP8 release

--- src/Interop.SolidEdge/Properties/AssemblyInfo.cs
+++ src/Interop.SolidEdge/Properties/AssemblyInfo.cs
@@ -38,4 +38,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("109.0.0.0")]
 
 // This version should match the NuGet version.
-[assembly: AssemblyFileVersion("109.7.0.0")]
+[assembly: AssemblyFileVersion("109.8.0.0")]
